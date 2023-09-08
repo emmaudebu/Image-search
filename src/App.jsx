ecab981f79b7ce1dc57c37239dbd4cda4f341bc1
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-09-08 16:45:41 +0100

    working on card components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img
+          src="https://source.unsplash.com/random"
+          alt=""
+          className=" w-full"
+        />
+      </div>
+    </main>
+  );
+}
+
+export default App;
