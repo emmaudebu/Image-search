8e3072c2f68b1bf3b964baf861aaea71a889d981
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-09-10 02:43:18 +0100

    done

--- src/Components/ImageCard.jsx
+++ src/Components/ImageCard.jsx
@@ -26,7 +26,7 @@ const ImageCard = ({ image }) => {
             </li>
           </ul>
         </div>
-        <div className="px-6 py-4">
+        <div className="px-2 py-4">
           {tags.map((tag, index) => (
             <span
               key={index}
